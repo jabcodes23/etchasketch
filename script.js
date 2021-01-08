720384f4c992071965084d9b10fb6fb2d7aa3441
jabcodes23 <jabwrites23@gmail.com>
2021-01-08 15:23:08 -0500

    Added basic sketching functionality

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-//Get container div
+// Get container div
 let container_div = document.getElementById('container_div');
+let drawingEnabled = false;
 
-//Create a 16x16 layout of squares
+// Add a listener to the container to toggle the drawing ability
+container_div.addEventListener('click', function (e) {
+    drawingEnabled = !drawingEnabled;
+});
+
+// Create a 16x16 layout of squares
+// Add the squares to the container div
+// Add a listener to each square
 for(var i = 0; i < 256; i++) {
     let grid_square = document.createElement('div');
-
-    grid_square.style.height = '50px';
-    grid_square.style.width = '50px';
-    grid_square.style.float = 'left';
-    grid_square.style.border = '1px solid black';
-
+    let id = 'square_' + i;
+    
+    grid_square.style.backgroundColor = 'white';
+    grid_square.id = id;
+    grid_square.classList.add('square');
+    
     container_div.appendChild(grid_square);
+
+    grid_square.addEventListener('mouseover', function(e) {
+        if(drawingEnabled) {
+            e.target.style.backgroundColor = 'black';
+        }
+    });
 }
