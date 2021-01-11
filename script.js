064a48aa15cde208b9a5a28f659700007103162f
jabcodes23 <jabwrites23@gmail.com>
2021-01-11 12:09:04 -0500

    Added the left and right divs and added some styling

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-// Get container div
-let container_div = document.getElementById('container_div');
-
-// Create and style logo div on left
-let left_div = document.createElement('div');
-left_div.style.float = 'left';
-left_div.style.backgroundColor = 'blue';
-left_div.style.height = '50px';
-left_div.style.width = '50px';
-document.body.appendChild(left_div);
-
-// Create and style options div on right
-let right_div = document.createElement('div');
-right_div.style.float = 'right';
-right_div.style.backgroundColor = 'blue';
-right_div.style.height = '50px';
-right_div.style.width = '50px';
-document.body.appendChild(right_div);
-
 // Diasble ability to sketch by default
 let sketchingEnabled = false;
+
+// Get container div
+let container_div = document.getElementById('container_div');
 
 // Add a listener to the container to toggle the sketching ability
 container_div.addEventListener('click', function (e) {
     sketchingEnabled = !sketchingEnabled;
 });
+
+// Create and style logo div on left
+let left_div = document.createElement('div');
+left_div.style.width = '33%';
+container_div.appendChild(left_div);
+
+// Create and center the pixel grid
+let grid_div = document.createElement('div');
+grid_div.classList.add('grid_div');
+grid_div.style.backgroundColor = 'black';
+grid_div.style.flex = 1;
+grid_div.style.width = 'min-content';
 
 // Use a loop to create a 16 by 16 grid of 'pixels' 
 for(var i = 0; i < 256; i++) {
@@ -36,7 +32,7 @@ for(var i = 0; i < 256; i++) {
     pixel.classList.add('square');
     
     // Add each pixel to the container div
-    container_div.appendChild(pixel);
+    grid_div.appendChild(pixel);
 
     // Add a listener to each pixel
     pixel.addEventListener('mouseover', function(e) {
@@ -45,3 +41,11 @@ for(var i = 0; i < 256; i++) {
         }
     });
 }
+
+// Add the pixel grid to the container div
+container_div.appendChild(grid_div);
+
+// Create and style options div on right
+let right_div = document.createElement('div');
+right_div.style.width = '33%';
+container_div.appendChild(right_div);
